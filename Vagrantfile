f1810a58b95803764e4535809beb2acbba20c98b
Dylan Vaughn <dylancvaughn@gmail.com>
2014-11-23 16:31:38 -0800

    uncommenting ros installation in vagrant bootstrap

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "janihur/ubuntu-1404-desktop"
-  config.vm.provision :shell, path: "ops/bootstrap.sh"
+  config.vm.provision :shell, path: "ops/vagrant/bootstrap.sh"
   config.vm.provider "virtualbox" do |v|
     v.gui = true
   end
--- ops/vagrant/bootstrap.sh
+++ ops/vagrant/bootstrap.sh
@@ -5,13 +5,13 @@ set -e
 # show all executing commands
 set -x
 
-# echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list
-# wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | apt-key add -
+echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | apt-key add -
 
-# echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
-# apt-get update
-# apt-get upgrade -y
-# apt-get install -y ros-indigo-desktop-full python-rosinstall
+echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
+apt-get update
+apt-get upgrade -y
+apt-get install -y ros-indigo-desktop-full python-rosinstall
 
 if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
     rosdep init
