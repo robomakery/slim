ac7be85ab7bfe10149e36ef597ca7602b984d089
Dylan Vaughn <dylancvaughn@gmail.com>
2014-12-21 14:55:06 -0800

    updating sourceme.bash

--- ros/sourceme.bash
+++ ros/sourceme.bash
@@ -4,5 +4,6 @@ ABSOLUTE_PATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
 SCRIPT_DIR=`dirname "$ABSOLUTE_PATH"`
 
 source devel/setup.bash
-export GAZEBO_MODEL_PATH=$SCRIPT_DIR/src/slim_gazebo/models
-export GAZEBO_RESOURCE_PATH=$SCRIPT_DIR/src/slim_gazebo/worlds
+export GAZEBO_MODEL_PATH=`rospack find slim_gazebo`/models
+export GAZEBO_RESOURCE_PATH=`rospack find slim_gazebo`/worlds
+
