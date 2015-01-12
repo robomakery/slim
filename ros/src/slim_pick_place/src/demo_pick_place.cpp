a0e881438d7e4ed336217999db177346b194328b
Dylan Vaughn <dylancvaughn@gmail.com>
2015-01-12 01:16:12 -0800

    WIP demo pick place

--- ros/src/slim_pick_place/src/demo_pick_place.cpp
+++ ros/src/slim_pick_place/src/demo_pick_place.cpp
@@ -40,6 +40,56 @@ int main(int argc, char **argv)
 
   ros::WallDuration(1.0).sleep();
 
+  moveit_msgs::CollisionObject collision_object;
+  collision_object.header.frame_id = group.getPlanningFrame();
+
+  /* The id of the object is used to identify it. */
+  collision_object.id = "box1";
+
+  /* Define a box to add to the world. */
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 0.4;
+  primitive.dimensions[1] = 0.1;
+  primitive.dimensions[2] = 0.4;
+
+  /* A pose for the box (specified relative to frame_id) */
+  geometry_msgs::Pose box_pose;
+  box_pose.orientation.w = 1.0;
+  box_pose.position.x =  0.6;
+  box_pose.position.y = -0.4;
+  box_pose.position.z =  1.2;
+
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
+
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collision_object);
+
+  ROS_INFO("Add an object into the world");
+  planning_scene_interface.addCollisionObjects(collision_objects);
+
+  /* Sleep so we have time to see the object in RViz */
+  sleep(2.0);
+
+  // try to move gantry
+  moveit::planning_interface::MoveGroup::Plan my_plan;
+  std::vector<double> group_variable_values;
+  group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()), group_variable_values);
+  group_variable_values[0] = 0.1;
+  group.setJointValueTarget(group_variable_values);
+  bool success = group.plan(my_plan);
+
+  ROS_INFO("Visualizing plan 2 (joint space goal) %s",success?"":"FAILED");
+  /* Sleep to give Rviz time to visualize the plan. */
+  sleep(5.0);
+  // move to the location
+  ROS_INFO("Moving to location");
+  group.move();
+  ROS_INFO("Done moving.");
+
   pick(group);
 
   ros::WallDuration(1.0).sleep();
