b54eb4017e5bcc865ea3f1a37865f1581b8aec48
Levi Lister <levi.lister@gmail.com>
2015-02-22 00:46:57 -0800

    Renamed variables in test_opencv_features.cpp

--- ros/src/slim_perception/src/test_opencv_features.cpp
+++ ros/src/slim_perception/src/test_opencv_features.cpp
@@ -14,66 +14,66 @@
 
 int main(int argc, char **argv)
 {
-  std::cout << "Template Image Matching Test" << std::endl;
+  std::cout << "Training Image Matching Test" << std::endl;
   
   if (argc != 3) {
     std::cerr << "Wrong # of input arguments!" << std::endl;
-    std::cerr << "Usage: " << argv[0] << " template.jpg target.jpg" << std::endl << std::endl;
+    std::cerr << "Usage: " << argv[0] << " training.jpg query.jpg" << std::endl << std::endl;
     return -1;
   }
 
   cv::ORB extractor;
-  std::vector<cv::KeyPoint> templateKeypoints, targetKeypoints;
-  cv::Mat templateDescriptors, targetDescriptors;
-  cv::Mat templateImageGray;
-  cv::Mat targetImageGray;
+  std::vector<cv::KeyPoint> trainingKeypoints, queryKeypoints;
+  cv::Mat trainingDescriptors, queryDescriptors;
+  cv::Mat trainingImageGray;
+  cv::Mat queryImageGray;
   cv::Mat imageWithKeypoints, imageWithKeypointsResized;
-  cv::Mat targetImageWithKeypoints, targetImageWithKeypointsResized;
+  cv::Mat queryImageWithKeypoints, queryImageWithKeypointsResized;
   const float scale = 0.25f;
 
-  // Read input template image 
-  cv::Mat templateImage = cv::imread(argv[1]);
+  // Read input training image 
+  cv::Mat trainingImage = cv::imread(argv[1]);
 
-  // Read input target image to match template against
-  cv::Mat targetImage = cv::imread(argv[2]);
+  // Read input query image to match training against
+  cv::Mat queryImage = cv::imread(argv[2]);
 
   // Convert to grayscale
-  cv::cvtColor(templateImage, templateImageGray, CV_BGR2GRAY);
-  cv::cvtColor(targetImage, targetImageGray, CV_BGR2GRAY);
+  cv::cvtColor(trainingImage, trainingImageGray, CV_BGR2GRAY);
+  cv::cvtColor(queryImage, queryImageGray, CV_BGR2GRAY);
 
   // Create fake mask image
-  cv::Mat maskImage = cv::Mat::ones(templateImage.size(), CV_8UC1);
-  cv::Mat targetMaskImage = cv::Mat::ones(targetImage.size(), CV_8UC1);
+  cv::Mat maskImage = cv::Mat::ones(trainingImage.size(), CV_8UC1);
+  cv::Mat queryMaskImage = cv::Mat::ones(queryImage.size(), CV_8UC1);
 
   // Feature/descriptor extraction
-  extractor(templateImageGray, maskImage, templateKeypoints, templateDescriptors);
-  extractor(targetImageGray, targetMaskImage, targetKeypoints, targetDescriptors);
-
+  extractor(trainingImageGray, maskImage, trainingKeypoints, trainingDescriptors);
+  extractor(queryImageGray, queryMaskImage, queryKeypoints, queryDescriptors);
 
   // Feature/descriptor matching
   cv::BFMatcher matcher(cv::NORM_HAMMING, true);
   std::vector<cv::DMatch> matches;
-  matcher.match(targetDescriptors, templateDescriptors, matches);
+  matcher.match(queryDescriptors, trainingDescriptors, matches);
 
   // Draw matches
   cv::Mat imageMatches, imageMatchesResized;
-  cv::drawMatches(targetImage, targetKeypoints, templateImage, templateKeypoints, matches, imageMatches);
+  cv::drawMatches(queryImage, queryKeypoints, trainingImage, trainingKeypoints, matches, imageMatches);
 
   // Draw keypoints
-//cv::drawKeypoints(templateImage, templateKeypoints, imageWithKeypoints);
-//cv::drawKeypoints(targetImage, targetKeypoints, targetImageWithKeypoints);
+//cv::drawKeypoints(trainingImage, trainingKeypoints, imageWithKeypoints);
+//cv::drawKeypoints(queryImage, queryKeypoints, queryImageWithKeypoints);
 
   // Display results
 //cv::resize(imageWithKeypoints, imageWithKeypointsResized,
 //           cv::Size(scale * imageWithKeypoints.cols, scale * imageWithKeypoints.rows));
-//cv::resize(targetImageWithKeypoints, targetImageWithKeypointsResized,
-//           cv::Size(scale * targetImageWithKeypoints.cols, scale * targetImageWithKeypoints.rows));
+//cv::resize(queryImageWithKeypoints, queryImageWithKeypointsResized,
+//           cv::Size(scale * queryImageWithKeypoints.cols, scale * queryImageWithKeypoints.rows));
   cv::resize(imageMatches, imageMatchesResized, 
              cv::Size(scale * imageMatches.cols, scale * imageMatches.rows));  
-//cv::imshow("Template Keypoints", imageWithKeypointsResized);
-//cv::imshow("Target Keypoints", targetImageWithKeypointsResized);
+//cv::imshow("Training Keypoints", imageWithKeypointsResized);
+//cv::imshow("Query Keypoints", queryImageWithKeypointsResized);
   cv::imshow("Matches", imageMatchesResized);
   cv::waitKey(0);
 
   return 0;
 }
+
