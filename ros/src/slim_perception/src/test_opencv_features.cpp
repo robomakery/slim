2424cb483d450956ae7604adeddb66d5109ec55e
Levi Lister <levi.lister@gmail.com>
2015-02-21 19:51:57 -0800

    Updated test_opencv_features.cpp with feature descriptor extraction and matching between template image and target image.

--- ros/src/slim_perception/src/test_opencv_features.cpp
+++ ros/src/slim_perception/src/test_opencv_features.cpp
@@ -16,39 +16,63 @@ int main(int argc, char **argv)
 {
   std::cout << "Template Image Matching Test" << std::endl;
   
-  if (argc != 2) {
+  if (argc != 3) {
     std::cerr << "Wrong # of input arguments!" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " template.jpg target.jpg" << std::endl << std::endl;
     return -1;
   }
+
+  cv::ORB extractor;
+  std::vector<cv::KeyPoint> templateKeypoints, targetKeypoints;
+  cv::Mat templateDescriptors, targetDescriptors;
+  cv::Mat templateImageGray;
+  cv::Mat targetImageGray;
+  cv::Mat imageWithKeypoints, imageWithKeypointsResized;
+  cv::Mat targetImageWithKeypoints, targetImageWithKeypointsResized;
+  const float scale = 0.25f;
 
   // Read input template image 
   cv::Mat templateImage = cv::imread(argv[1]);
 
-  // Convert template image to grayscale
-  cv::Mat templateImageGray;
+  // Read input target image to match template against
+  cv::Mat targetImage = cv::imread(argv[2]);
+
+  // Convert to grayscale
   cv::cvtColor(templateImage, templateImageGray, CV_BGR2GRAY);
+  cv::cvtColor(targetImage, targetImageGray, CV_BGR2GRAY);
 
   // Create fake mask image
   cv::Mat maskImage = cv::Mat::ones(templateImage.size(), CV_8UC1);
+  cv::Mat targetMaskImage = cv::Mat::ones(targetImage.size(), CV_8UC1);
 
-  // Print image image
-  std::cout << "Template Image (Gray):\n";
-  std::cout << "  Size: " << templateImageGray.cols << " x " << templateImageGray.rows << std::endl;
-  std::cout << "  Depth: " << templateImageGray.depth() << ", Channels: " << templateImageGray.channels() << ", Type: " << templateImageGray.type() << std::endl;
+  // Feature/descriptor extraction
+  extractor(templateImageGray, maskImage, templateKeypoints, templateDescriptors);
+  extractor(targetImageGray, targetMaskImage, targetKeypoints, targetDescriptors);
 
-  // OpenCV ORB feature extractor
-  cv::ORB orbFeatureExtractor;
-  std::vector<cv::KeyPoint> keypoints;
-  orbFeatureExtractor(templateImageGray, maskImage, keypoints);
+
+  // Feature/descriptor matching
+  cv::BFMatcher matcher(cv::NORM_HAMMING, true);
+  std::vector<cv::DMatch> matches;
+  matcher.match(targetDescriptors, templateDescriptors, matches);
+
+  // Draw matches
+  cv::Mat imageMatches, imageMatchesResized;
+  cv::drawMatches(targetImage, targetKeypoints, templateImage, templateKeypoints, matches, imageMatches);
 
   // Draw keypoints
-  cv::Mat imageWithKeypoints, imageWithKeypointsResized;
-  cv::drawKeypoints(templateImage, keypoints, imageWithKeypoints);
+//cv::drawKeypoints(templateImage, templateKeypoints, imageWithKeypoints);
+//cv::drawKeypoints(targetImage, targetKeypoints, targetImageWithKeypoints);
 
-  // Display keypoints
-  const float scale = 0.5f;
-  cv::resize(imageWithKeypoints, imageWithKeypointsResized, cv::Size(scale * imageWithKeypoints.cols, scale * imageWithKeypoints.rows));
-  cv::imshow("Keypoints", imageWithKeypointsResized);
+  // Display results
+//cv::resize(imageWithKeypoints, imageWithKeypointsResized,
+//           cv::Size(scale * imageWithKeypoints.cols, scale * imageWithKeypoints.rows));
+//cv::resize(targetImageWithKeypoints, targetImageWithKeypointsResized,
+//           cv::Size(scale * targetImageWithKeypoints.cols, scale * targetImageWithKeypoints.rows));
+  cv::resize(imageMatches, imageMatchesResized, 
+             cv::Size(scale * imageMatches.cols, scale * imageMatches.rows));  
+//cv::imshow("Template Keypoints", imageWithKeypointsResized);
+//cv::imshow("Target Keypoints", targetImageWithKeypointsResized);
+  cv::imshow("Matches", imageMatchesResized);
   cv::waitKey(0);
 
   return 0;
